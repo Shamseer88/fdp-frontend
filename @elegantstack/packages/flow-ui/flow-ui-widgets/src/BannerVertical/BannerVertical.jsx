eafb730c1822c42a334b3d9e77f8d3f2a218c246
kalai <kalai@brandanswers.in>
2022-07-15 13:27:38 +0530

    initial commit

---
+++ @elegantstack/packages/flow-ui/flow-ui-widgets/src/BannerVertical/BannerVertical.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { Flex, Button, Heading, Text } from 'theme-ui'
+import { FaRegHandshake } from 'react-icons/fa'
+
+const styles = {
+  wrapper: {
+    alignItems: `center`,
+    flexDirection: `column`,
+    bg: `omegaLighter`,
+    borderRadius: `lg`,
+    size: `full`,
+    p: 4,
+  },
+  heading: {
+    color: `omegaDark`,
+    wordSpacing: 500, //force line breaks
+    svg: {
+      color: `beta`,
+      size: `icon.lg`,
+      display: `block`,
+      mb: 3,
+    },
+  },
+  subheading: {
+    color: `omegaDark`,
+    fontWeight: `normal`,
+  },
+  list: {
+    color: `omegaDark`,
+    listStyle: `none`,
+    m: 0,
+    p: 0,
+    li: {
+      p: 0,
+      my: 2,
+    },
+    'li:before': {
+      content: `""`,
+      display: `inline-block`,
+      width: `icon.xs`,
+      bg: `success`,
+      borderRadius: `full`,
+      size: `7px`,
+      mr: `7px`,
+    },
+  },
+  button: {
+    display: `block`,
+    mt: `auto`,
+  },
+}
+
+const BannerVertical = () => (
+  <Flex sx={styles.wrapper}>
+    <Heading variant='h2' sx={styles.heading}>
+      <FaRegHandshake />
+UAE    </Heading>
+    <Heading variant='h4' sx={styles.subheading}>
+     TBD
+    </Heading>
+    <Text as='ul' variant='small' sx={styles.list}>
+      <li>TBDg</li>
+      <li>TBD</li>
+      <li>TBD</li>
+    </Text>
+    <Button
+      variant='primary'
+      as={Link}
+      to='/'
+      sx={styles.button}
+      aria-label='Learn More'
+    >
+      Learn More
+    </Button>
+  </Flex>
+)
+
+export default BannerVertical
